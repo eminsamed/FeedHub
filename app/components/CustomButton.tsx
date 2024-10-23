7ab6224d58e37fb124c113e6aed8db7cfb000b28
eminsamed <emin.yilmaz@powercoders.org>
2024-10-23 23:06:46 +0200

    fix: Refactored CustomButton component to use children prop for flexibility

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -1,17 +1,16 @@
-"use client";
-
 import Button from "@mui/material/Button";
 
 interface CustomButtonProps {
-  onClick: () => void;
-  label: string;
-  variant?: "text" | "outlined" | "contained"; // Optional prop for button variant
+  onClick?: () => void;
+  variant?: "text" | "outlined" | "contained";
+  type?: "button" | "submit" | "reset";
+  children: React.ReactNode;
 }
 
-export default function CustomButton({ onClick, label, variant = "contained" }: CustomButtonProps) {
+export default function CustomButton({ onClick, variant = "contained", type = "button", children }: CustomButtonProps) {
   return (
-    <Button variant={variant} onClick={onClick}>
-      {label}
+    <Button onClick={onClick} variant={variant} type={type}>
+      {children}
     </Button>
   );
 }
