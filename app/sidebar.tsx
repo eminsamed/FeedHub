cad15a9028a125f0b0b56fcb6ab58ee7e197c0fe
eminsamed <emin.yilmaz@powercoders.org>
2024-11-27 18:44:04 +0100

    Removed 'Edit Feedback' option from sidebar and dashboard as it's covered by 'View Feedback'

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -6,129 +6,32 @@ export default function Sidebar() {
   return (
     <>
       <div className="bg-gray-100 text-black">
-        {/* Sidebar */}
-        FINAL LINKS COME HERE:
+        {/* Feedbacks Section */}
         <aside className="w-64 p-4">
           <nav>
             <ul>
               <li className="mb-2">
-                <Link
-                  href="/applications"
-                  className="text-gray-700 hover:underline"
-                >
-                  Applications
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="/access-groups"
-                  className="text-gray-700 hover:underline"
-                >
-                  Access groups
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        STUCTURE (for development)
-        <aside className="w-64 bg-gray-100 p-4">
-          <nav>
-            <ul>
-              {/* Applications section */}
-              <li className="mb-2">
-                <Link
-                  href="/applications"
-                  className="text-gray-700 hover:underline"
-                >
-                  Applications
-                </Link>
-                <ul className="pl-4 mt-2">
-                  <li className="mb-1">
-                    <Link
-                      href="/applications/add"
-                      className="text-gray-600 hover:underline"
-                    >
-                      Add
-                    </Link>
-                  </li>
-                  <li className="mb-1">
-                    <Link
-                      href="/applications/edit"
-                      className="text-gray-600 hover:underline"
-                    >
-                      Edit
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-
-              {/* Feedbacks section */}
-              <li className="mb-2">
-                <Link
-                  href="/feedbacks"
-                  className="text-gray-700 hover:underline"
-                >
+                <Link href="/feedbacks" className="text-gray-700 hover:underline">
                   Feedbacks
                 </Link>
                 <ul className="pl-4 mt-2">
                   <li className="mb-1">
-                    <Link
-                      href="/feedbacks/add"
-                      className="text-gray-600 hover:underline"
-                    >
+                    <Link href="/feedbacks/add" className="text-gray-600 hover:underline">
                       Add
                     </Link>
                   </li>
                   <li className="mb-1">
-                    <Link
-                      href="/feedbacks/edit"
-                      className="text-gray-600 hover:underline"
-                    >
-                      Edit
-                    </Link>
-                  </li>
-                  <li className="mb-1">
-                    <Link
-                      href="/feedbacks/dashboard"
-                      className="text-gray-600 hover:underline"
-                    >
+                    <Link href="/feedbacks/dashboard" className="text-gray-600 hover:underline">
                       Dashboard
                     </Link>
                   </li>
                 </ul>
               </li>
-
-              {/* Access Groups section */}
-              <li className="mb-2">
-                <Link
-                  href="/access-groups"
-                  className="text-gray-700 hover:underline"
-                >
-                  Access Groups
-                </Link>
-                <ul className="pl-4 mt-2">
-                  <li className="mb-1">
-                    <Link
-                      href="/access-groups/edit"
-                      className="text-gray-600 hover:underline"
-                    >
-                      Edit
-                    </Link>
-                  </li>
-                  <li className="mb-1">
-                    <Link
-                      href="/access-groups/add"
-                      className="text-gray-600 hover:underline"
-                    >
-                      Add
-                    </Link>
-                  </li>
-                </ul>
-              </li>
             </ul>
           </nav>
         </aside>
-        OTHER TEST LINKS
+
+        {/* Other Test Links */}
         <aside className="w-64 bg-gray-100 p-4">
           <nav>
             <ul>
@@ -138,18 +41,7 @@ export default function Sidebar() {
                 </Link>
               </li>
               <li className="mb-2">
-                <Link
-                  href="/experimental-page"
-                  className="text-gray-700 hover:underline"
-                >
-                  Experimental Page
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link
-                  href="/404notfoundpagethatdoesntexist"
-                  className="text-gray-700 hover:underline"
-                >
+                <Link href="/404notfoundpagethatdoesntexist" className="text-gray-700 hover:underline">
                   404 not found
                 </Link>
               </li>
